d9483c9d20be46a4a196bc7ae92f5f43271a2ccd
Dylan Musil <dylanmusil@gmail.com>
2022-09-05 08:21:08 -0400

    Refactor to common base class.

---
+++ ModelingCanvas.Domain/CanvasObjects.cs
@@ -0,0 +1,20 @@
+﻿namespace ModelingCanvas.Domain;
+
+public abstract record LinkedObject<T> 
+{
+    public T? Next { get; protected set; }
+    public void Link(T next) => Next = next;
+    public void Unlink() => Next = default;
+}
+
+public record Command : LinkedObject<Event>;
+
+public record Event : LinkedObject<ReadModel>;
+
+public record ReadModel : LinkedObject<CommandIssuer>;
+
+public abstract record CommandIssuer : LinkedObject<Command>;
+
+public record Automation : CommandIssuer;
+
+public record UserInterface : CommandIssuer;
--- ModelingCanvas.Tests/UnitTest1.cs
+++ ModelingCanvas.Tests/UnitTest1.cs
@@ -1,90 +1,89 @@
 using ModelingCanvas.Domain;
 
-namespace ModelingCanvas.Tests
+namespace ModelingCanvas.Tests;
+
+public class Tests
 {
-    public class Tests
+    [SetUp]
+    public void Setup()
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+    }
 
-        [Test]
-        public void LinkEventToCommand()
-        {
-            var cmd = new Command();
-            var @event = new Event();
-            cmd.Link(@event);
+    [Test]
+    public void LinkEventToCommand()
+    {
+        var cmd = new Command();
+        var @event = new Event();
+        cmd.Link(@event);
 
-            Assert.That(cmd.Event, Is.EqualTo(@event));
+        Assert.That(cmd.Next, Is.EqualTo(@event));
 
-            cmd.Unlink(@event);
-            Assert.That(cmd.Event, Is.Null);
-        }
+        cmd.Unlink();
+        Assert.That(cmd.Next, Is.Null);
+    }
 
-        [Test]
-        public void LinkEventToReadModel()
-        {
-            var evt = new Event();
-            var readModel = new ReadModel();
-            evt.Link(readModel);
+    [Test]
+    public void LinkEventToReadModel()
+    {
+        var evt = new Event();
+        var readModel = new ReadModel();
+        evt.Link(readModel);
 
-            Assert.That(evt.ReadModel, Is.EqualTo(readModel));
+        Assert.That(evt.Next, Is.EqualTo(readModel));
 
-            evt.Unlink(readModel);
-            Assert.That(evt.ReadModel, Is.Null);
-        }
+        evt.Unlink();
+        Assert.That(evt.Next, Is.Null);
+    }
 
-        [Test]
-        public void LinkReadModelToAutomation()
-        {
-            var auto = new Automation();
-            var readModel = new ReadModel();
-            readModel.Link(auto);
+    [Test]
+    public void LinkReadModelToAutomation()
+    {
+        var auto = new Automation();
+        var readModel = new ReadModel();
+        readModel.Link(auto);
 
-            Assert.That(readModel.CommandIssuer, Is.EqualTo(auto));
+        Assert.That(readModel.Next, Is.EqualTo(auto));
 
-            readModel.Unlink(auto);
-            Assert.That(readModel.CommandIssuer, Is.Null);
-        }
+        readModel.Unlink();
+        Assert.That(readModel.Next, Is.Null);
+    }
 
-        [Test]
-        public void LinkReadModelToUserInterface()
-        {
-            var ui = new UserInterface();
-            var readModel = new ReadModel();
-            readModel.Link(ui);
+    [Test]
+    public void LinkReadModelToUserInterface()
+    {
+        var ui = new UserInterface();
+        var readModel = new ReadModel();
+        readModel.Link(ui);
 
-            Assert.That(readModel.CommandIssuer, Is.EqualTo(ui));
+        Assert.That(readModel.Next, Is.EqualTo(ui));
 
-            readModel.Unlink(ui);
-            Assert.That(readModel.CommandIssuer, Is.Null);
-        }
+        readModel.Unlink();
+        Assert.That(readModel.Next, Is.Null);
+    }
 
-        [Test]
-        public void LinkUserInterfaceToCommand()
-        {
-            var cmd = new Command();
-            var ui = new UserInterface();
-            ui.Link(cmd);
+    [Test]
+    public void LinkUserInterfaceToCommand()
+    {
+        var cmd = new Command();
+        var ui = new UserInterface();
+        ui.Link(cmd);
 
-            Assert.That(ui.Command, Is.EqualTo(cmd));
+        Assert.That(ui.Next, Is.EqualTo(cmd));
 
-            ui.Unlink(cmd);
-            Assert.That(ui.Command, Is.Null);
-        }
+        ui.Unlink();
+        Assert.That(ui.Next, Is.Null);
+    }
 
-        [Test]
-        public void LinkAutomationToCommand()
-        {
-            var cmd = new Command();
-            var auto = new Automation();
-            auto.Link(cmd);
+    [Test]
+    public void LinkAutomationToCommand()
+    {
+        var cmd = new Command();
+        var auto = new Automation();
+        auto.Link(cmd);
 
-            Assert.That(auto.Command, Is.EqualTo(cmd));
+        Assert.That(auto.Next, Is.EqualTo(cmd));
 
-            auto.Unlink(cmd);
-            Assert.That(auto.Command, Is.Null);
-        }
+        auto.Unlink();
+        Assert.That(auto.Next, Is.Null);
     }
 }
