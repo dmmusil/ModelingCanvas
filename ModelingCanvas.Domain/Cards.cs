8231d814988e49385c59d95bf9bd9af9e0b84d71
Dylan Musil <dylanmusil@gmail.com>
2022-09-09 19:38:15 -0400

    Redo model with no generics

---
+++ ModelingCanvas.Domain/Cards.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+
+namespace ModelingCanvas.Domain;
+
+public abstract record Card(Guid Id)
+{
+    public abstract bool CanLinkTo(Card card);
+}
+
+public record Command(Guid Id) : Card(Id)
+{
+    public override bool CanLinkTo(Card card) => card is Event;
+}
+
+public record Event(Guid Id) : Card(Id)
+{
+    public override bool CanLinkTo(Card card) => card is View;
+}
+
+public record View(Guid Id) : Card(Id)
+{
+    public override bool CanLinkTo(Card card) => card is UserInterface or Automation;
+}
+
+public record UserInterface(Guid Id) : Card(Id)
+{
+    public override bool CanLinkTo(Card card) => card is Command;
+}
+
+public record Automation(Guid Id) : Card(Id)
+{
+    public override bool CanLinkTo(Card card) => card is Command;
+}
+
+public record Link(Card Source, Card Destination);
+
+public record Board(Guid Id)
+{
+    private readonly Dictionary<Guid, Card> _cards = new();
+    private readonly List<Link> _links = new();
+    public IEnumerable<Card> Cards => _cards.Values;
+
+    public void Add(Card card) => _cards[card.Id] = card;
+
+    public void LinkBetween(Card source, Card destination)
+    {
+        if (source.CanLinkTo(destination))
+        {
+            _links.Add(new Link(_cards[source.Id], _cards[destination.Id]));
+        }
+    }
+
+    public bool AreLinked(Card source, Card destination) =>
+        _links.Contains(new Link(source, destination));
+
+    public void DeleteLink(Card source, Card destination) => 
+        _links.Remove(new Link(source, destination));
+
+    public void DeleteCard(Card card)
+    {
+        _cards.Remove(card.Id);
+        _links.RemoveAll(link => link.Source.Id == card.Id || link.Destination.Id == card.Id);
+    }
+}
---
+++ ModelingCanvas.Tests/BoardTests.cs
@@ -0,0 +1,60 @@
+﻿using ModelingCanvas.Domain;
+
+namespace ModelingCanvas.Tests;
+
+public class BoardTests
+{
+    [Test]
+    public void CommandToEvent()
+    {
+        var board = new Board(Guid.NewGuid());
+        var cmd = new Command(Guid.NewGuid());
+        var evt = new Event(Guid.NewGuid());
+
+        board.Add(cmd);
+        board.Add(evt);
+        board.LinkBetween(cmd, evt);
+
+        Assert.That(board.AreLinked(cmd, evt));
+
+        board.DeleteLink(cmd, evt);
+     
+        Assert.That(board.AreLinked(cmd, evt), Is.False);
+    }
+
+    [Test]
+    public void EventToView()
+    {
+        var board = new Board(Guid.NewGuid());
+        var view = new View(Guid.NewGuid());
+        var evt = new Event(Guid.NewGuid());
+
+        board.Add(view);
+        board.Add(evt);
+        board.LinkBetween( evt, view);
+
+        Assert.That(board.AreLinked( evt, view));
+
+        board.DeleteLink(evt, view);
+
+        Assert.That(board.AreLinked(evt, view), Is.False);
+    }
+
+    [Test]
+    public void DeletingLinkedCardRemovesLink()
+    {
+        var board = new Board(Guid.NewGuid());
+        var view = new View(Guid.NewGuid());
+        var evt = new Event(Guid.NewGuid());
+
+        board.Add(view);
+        board.Add(evt);
+        board.LinkBetween(evt, view);
+
+        Assert.That(board.AreLinked(evt, view));
+
+        board.DeleteCard(evt);
+
+        Assert.That(board.AreLinked(evt, view), Is.False);
+    }
+}
