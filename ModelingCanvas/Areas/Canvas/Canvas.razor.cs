26ea709a4566ae985ec280e930eb47501094b8f1
Dylan Musil <dylanmusil@gmail.com>
2022-09-10 10:20:51 -0400

    Add and delete cards to UI

---
+++ ModelingCanvas/Areas/Canvas/Canvas.razor.cs
@@ -0,0 +1,8 @@
+﻿using ModelingCanvas.Domain;
+
+namespace ModelingCanvas.Areas.Canvas;
+
+public partial class Canvas
+{
+    private readonly Board _board = new Board(Guid.NewGuid());
+}
---
+++ ModelingCanvas/Areas/Canvas/Card.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ModelingCanvas.Areas.Canvas;
+
+public partial class Card
+{
+
+    [Parameter]
+    public Domain.Card Model { get; set; }
+
+    [Parameter]
+    public EventCallback OnDelete { get; set; }
+}
